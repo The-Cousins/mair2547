21dd724a243338e007f939c2e93109f5bd500d1b
George Mihoc <george.mihoc@gmail.com>
2021-03-27 13:56:37 +0200

    Added BVA Tests + some refactoring

--- 02_Inventory/src/main/java/inventory/service/InventoryServiceTest.java
+++ 02_Inventory/src/main/java/inventory/service/InventoryServiceTest.java
@@ -28,16 +28,17 @@ class InventoryServiceTest {
         writer.close();
     }
 
+//    ECP
     @org.junit.jupiter.api.Test
     void addInhousePart() {
-        addInhouseValidPart();
-        addInhouseInvalidNamePart();
-        addInhouseStockLowerThanMinPart();
-        addInhouseStockGreaterThanMaxPart();
+        addInhouseValidPartTest1();
+        addInhouseInvalidNamePartTest2();
+        addInhouseStockLowerThanMinPartTest10();
+        addInhouseStockGreaterThanMaxPartTest11();
     }
 
     @org.junit.jupiter.api.Test
-    void addInhouseValidPart() {
+    void addInhouseValidPartTest1() {
         try {
             String name = "surub";
             int price = 5;
@@ -51,23 +52,23 @@ class InventoryServiceTest {
 
             List<Part> allParts = service.getAllParts();
             // Assert
-            assert allParts.get(0).getName().equals(name);
-            assert allParts.get(0).getPrice() == price;
-            assert allParts.get(0).getInStock() == inStock;
-            assert allParts.get(0).getMin() == min;
-            assert allParts.get(0).getMax() == max;
+            assert allParts.get(allParts.size()-1).getName().equals(name);
+            assert allParts.get(allParts.size()-1).getPrice() == price;
+            assert allParts.get(allParts.size()-1).getInStock() == inStock;
+            assert allParts.get(allParts.size()-1).getMin() == min;
+            assert allParts.get(allParts.size()-1).getMax() == max;
         } catch (Exception exception) {
             assert false;
         }
     }
 
     @org.junit.jupiter.api.Test
-    void addInhouseInvalidNamePart() {
+    void addInhouseInvalidNamePartTest2() {
         try {
             String name = "";
             int price = 5;
-            int inStock = 10;
-            int min = 5;
+            int inStock = 100;
+            int min = 50;
             int max = 200;
             int partDynamicValue = 29381;
 
@@ -80,11 +81,11 @@ class InventoryServiceTest {
     }
 
     @org.junit.jupiter.api.Test
-    void addInhouseStockLowerThanMinPart() {
+    void addInhouseStockLowerThanMinPartTest10() {
         try {
             String name = "surub";
             int price = 5;
-            int inStock = 25;
+            int inStock = 49;
             int min = 50;
             int max = 200;
             int partDynamicValue = 19928;
@@ -98,11 +99,11 @@ class InventoryServiceTest {
     }
 
     @org.junit.jupiter.api.Test
-    void addInhouseStockGreaterThanMaxPart() {
+    void addInhouseStockGreaterThanMaxPartTest11() {
         try {
             String name = "surub";
             int price = 5;
-            int inStock = 500;
+            int inStock = 201;
             int min = 50;
             int max = 200;
             int partDynamicValue = 19928;
@@ -114,4 +115,239 @@ class InventoryServiceTest {
             assert exception.getMessage().equals("Inventory level is higher than the maximum value. ");
         }
     }
+
+//    BVA
+    @org.junit.jupiter.api.Test
+    void addInhousePartBVA() {
+        addInhouseInvalidNamePartBVATest1();
+        addInhouseValidPartBVATest2();
+        addInhouseValidPartBVATest3();
+        addInhouseValidPartBVATest4();
+        addInhouseValidPartBVATest5();
+
+        addInhouseValidPartBVATest26();
+        addInhouseValidPartBVATest27();
+        addInhouseValidPartBVATest28();
+        addInhouseValidPartBVATest29();
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInhouseInvalidNamePartBVATest1() {
+        try {
+            String name = "";
+            int price = 5;
+            int inStock = 100;
+            int min = 50;
+            int max = 200;
+            int partDynamicValue = 29381;
+
+            // Act
+            service.addInhousePart(name, price, inStock, min, max, partDynamicValue);
+            assert false;
+        } catch (Exception exception) {
+            // Assert
+            assert exception.getMessage().equals("A name has not been entered. ");
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInhouseValidPartBVATest2() {
+        try {
+            String name = "t";
+            int price = 5;
+            int inStock = 100;
+            int min = 50;
+            int max = 200;
+            int partDynamicValue = 29381;
+
+            // Act
+            service.addInhousePart(name, price, inStock, min, max, partDynamicValue);
+
+            List<Part> allParts = service.getAllParts();
+            // Assert
+            assert allParts.get(allParts.size()-1).getName().equals(name);
+            assert allParts.get(allParts.size()-1).getPrice() == price;
+            assert allParts.get(allParts.size()-1).getInStock() == inStock;
+            assert allParts.get(allParts.size()-1).getMin() == min;
+            assert allParts.get(allParts.size()-1).getMax() == max;
+        } catch (Exception exception) {
+            assert false;
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInhouseValidPartBVATest3() {
+        try {
+            String name = "te";
+            int price = 5;
+            int inStock = 100;
+            int min = 50;
+            int max = 200;
+            int partDynamicValue = 29381;
+
+            // Act
+            service.addInhousePart(name, price, inStock, min, max, partDynamicValue);
+
+            List<Part> allParts = service.getAllParts();
+            // Assert
+            assert allParts.get(allParts.size()-1).getName().equals(name);
+            assert allParts.get(allParts.size()-1).getPrice() == price;
+            assert allParts.get(allParts.size()-1).getInStock() == inStock;
+            assert allParts.get(allParts.size()-1).getMin() == min;
+            assert allParts.get(allParts.size()-1).getMax() == max;
+        } catch (Exception exception) {
+            assert false;
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInhouseValidPartBVATest4() {
+        try {
+            String name = "te...12";
+            int price = 5;
+            int inStock = 100;
+            int min = 50;
+            int max = 200;
+            int partDynamicValue = 29381;
+
+            // Act
+            service.addInhousePart(name, price, inStock, min, max, partDynamicValue);
+
+            List<Part> allParts = service.getAllParts();
+            // Assert
+            assert allParts.get(allParts.size()-1).getName().equals(name);
+            assert allParts.get(allParts.size()-1).getPrice() == price;
+            assert allParts.get(allParts.size()-1).getInStock() == inStock;
+            assert allParts.get(allParts.size()-1).getMin() == min;
+            assert allParts.get(allParts.size()-1).getMax() == max;
+        } catch (Exception exception) {
+            assert false;
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInhouseValidPartBVATest5() {
+        try {
+            String name = "te...123";
+            int price = 5;
+            int inStock = 100;
+            int min = 50;
+            int max = 200;
+            int partDynamicValue = 29381;
+
+            // Act
+            service.addInhousePart(name, price, inStock, min, max, partDynamicValue);
+
+            List<Part> allParts = service.getAllParts();
+            // Assert
+            assert allParts.get(allParts.size()-1).getName().equals(name);
+            assert allParts.get(allParts.size()-1).getPrice() == price;
+            assert allParts.get(allParts.size()-1).getInStock() == inStock;
+            assert allParts.get(allParts.size()-1).getMin() == min;
+            assert allParts.get(allParts.size()-1).getMax() == max;
+        } catch (Exception exception) {
+            assert false;
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInhouseValidPartBVATest26() {
+        try {
+            String name = "surub";
+            int price = 5;
+            int inStock = 50;
+            int min = 50;
+            int max = 200;
+            int partDynamicValue = 19928;
+
+            // Act
+            service.addInhousePart(name, price, inStock, min, max, partDynamicValue);
+
+            List<Part> allParts = service.getAllParts();
+            // Assert
+            assert allParts.get(allParts.size()-1).getName().equals(name);
+            assert allParts.get(allParts.size()-1).getPrice() == price;
+            assert allParts.get(allParts.size()-1).getInStock() == inStock;
+            assert allParts.get(allParts.size()-1).getMin() == min;
+            assert allParts.get(allParts.size()-1).getMax() == max;
+        } catch (Exception exception) {
+            assert false;
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInhouseValidPartBVATest27() {
+        try {
+            String name = "surub";
+            int price = 5;
+            int inStock = 51;
+            int min = 50;
+            int max = 200;
+            int partDynamicValue = 19928;
+
+            // Act
+            service.addInhousePart(name, price, inStock, min, max, partDynamicValue);
+
+            List<Part> allParts = service.getAllParts();
+            // Assert
+            assert allParts.get(allParts.size()-1).getName().equals(name);
+            assert allParts.get(allParts.size()-1).getPrice() == price;
+            assert allParts.get(allParts.size()-1).getInStock() == inStock;
+            assert allParts.get(allParts.size()-1).getMin() == min;
+            assert allParts.get(allParts.size()-1).getMax() == max;
+        } catch (Exception exception) {
+            assert false;
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInhouseValidPartBVATest28() {
+        try {
+            String name = "surub";
+            int price = 5;
+            int inStock = 199;
+            int min = 50;
+            int max = 200;
+            int partDynamicValue = 19928;
+
+            // Act
+            service.addInhousePart(name, price, inStock, min, max, partDynamicValue);
+
+            List<Part> allParts = service.getAllParts();
+            // Assert
+            assert allParts.get(allParts.size()-1).getName().equals(name);
+            assert allParts.get(allParts.size()-1).getPrice() == price;
+            assert allParts.get(allParts.size()-1).getInStock() == inStock;
+            assert allParts.get(allParts.size()-1).getMin() == min;
+            assert allParts.get(allParts.size()-1).getMax() == max;
+        } catch (Exception exception) {
+            assert false;
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void addInhouseValidPartBVATest29() {
+        try {
+            String name = "surub";
+            int price = 5;
+            int inStock = 200;
+            int min = 50;
+            int max = 200;
+            int partDynamicValue = 19928;
+
+            // Act
+            service.addInhousePart(name, price, inStock, min, max, partDynamicValue);
+
+            List<Part> allParts = service.getAllParts();
+            // Assert
+            assert allParts.get(allParts.size()-1).getName().equals(name);
+            assert allParts.get(allParts.size()-1).getPrice() == price;
+            assert allParts.get(allParts.size()-1).getInStock() == inStock;
+            assert allParts.get(allParts.size()-1).getMin() == min;
+            assert allParts.get(allParts.size()-1).getMax() == max;
+        } catch (Exception exception) {
+            assert false;
+        }
+    }
+
 }
