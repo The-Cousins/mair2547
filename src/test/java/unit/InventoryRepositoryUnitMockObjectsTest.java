73004370ad3470344a45a424ba904b2bd56de363
George Mihoc <george.mihoc@gmail.com>
2021-04-22 19:04:17 +0300

    Refactored class names

--- src/test/java/unit/InventoryRepositoryUnitMockObjectsTest.java
+++ src/test/java/unit/InventoryRepositoryUnitMockObjectsTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 
-class InventoryRepositoryUnitTestWithMockObjects {
+class InventoryRepositoryUnitMockObjectsTest {
 
     Inventory inventory;
     InventoryRepository repository;
--- src/test/java/unit/InventoryServiceUnitMockObjectsTest.java
+++ src/test/java/unit/InventoryServiceUnitMockObjectsTest.java
@@ -20,7 +20,7 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class InventoryServiceUnitTestWithMockObjects {
+class InventoryServiceUnitMockObjectsTest {
 
     InventoryRepository repo;
     InventoryService service;
